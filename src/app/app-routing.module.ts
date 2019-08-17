bb3714cc2baac464b12f59ff07d4469d114474ae
jguillen2017076 <jeanfguillen27@gmail.com>
2019-08-17 11:42:22 -0600

    PrimerasVistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { TipoEmpaquesComponent } from './components/tipo-empaques/tipo-empaques.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'categorias', component: CategoriasComponent},
+  {path: 'tipoEmpaque', component: TipoEmpaquesComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { TipoEmpaquesComponent } from './components/tipo-empaques/tipo-empaques.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    CategoriasComponent,
+    TipoEmpaquesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
